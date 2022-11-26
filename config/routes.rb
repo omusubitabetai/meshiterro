da75949a9f48b34ecb613c5e802a3110789b0887
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2022-11-26 14:38:42 +0000

    [Add]5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   devise_for :users
-root to: "homes#top"
+  
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "homes/about"
 end
