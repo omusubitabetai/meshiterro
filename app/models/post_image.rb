9fe66bba780e68b73500aaa2d134655e87c5c462
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2022-11-27 14:56:51 +0000

    [Add] 5-23

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,22 @@
 class PostImage < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
-   has_one_attached :image
-   belongs_to :user
+  validates :shop_name, presence: true
+  validates :image, presence: true
+  
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
 end
